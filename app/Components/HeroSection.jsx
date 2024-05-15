e2657d08c4a2a887df32d6acafb29b26f36b30bf
KOrtizLedezma <kenethortiz2016@gmail.com>
2024-05-15 18:31:33 -0400

    image size changed

--- app/Components/HeroSection.jsx
+++ app/Components/HeroSection.jsx
@@ -77,8 +77,8 @@ const HeroSection = () => {
               src="/Images/hero-image.png"
               alt=""
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={200}
-              height={200}
+              width={250}
+              height={250}
             />
           </div>
         </motion.div>
