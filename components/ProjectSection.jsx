07268e8fdc08fbb27cc1812bcf0980daaad4a77f
KOrtizLedezma <kenethortiz2016@gmail.com>
2025-05-01 01:31:22 -0400

    Added 3 new projects

--- components/ProjectSection.jsx
+++ components/ProjectSection.jsx
@@ -7,6 +7,15 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
+    title: "IMDB",
+    description: "A lightweight, thread-safe in-memory key-value store written in C++.",
+    image: "/Images/Projects/IMDB.png",
+    tag: ["All", "C++"],
+    gitUrl: "https://github.com/KOrtizLedezma/IMDB.git", 
+    demoUrl: null,
+  },
+  {
+    id: 2,
     title: "Task Manager - Website",
     description: "A minimalist task management web app that helps users organize, schedule, and complete daily tasks with a clean, responsive interface and secure authentication.",
     image: "/Images/Projects/TaskManager.png",
@@ -15,7 +24,7 @@ const projectsData = [
     demoUrl: null,
   },
   {
-    id: 2,
+    id: 3,
     title: "Song Association Helper",
     description: "A simple and responsive app that suggests songs according to a word.",
     image: "/Images/Projects/SongAssociation_adj.png",
@@ -24,7 +33,7 @@ const projectsData = [
     demoUrl: "https://songassociationhelper.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Geoquiz",
     description: "GeoQuiz is an interactive platform designed to make learning geography fun and engaging through quizzes.",
     image: "/Images/Projects/Geoquiz.png",
@@ -33,7 +42,7 @@ const projectsData = [
     demoUrl: null,
   },
   {
-    id: 4,
+    id: 5,
     title: "The Moderator Bot",
     description: "A simple discord bot, focused on small servers.",
     image: "/Images/Projects/DiscordWebsite.png",
@@ -43,7 +52,7 @@ const projectsData = [
   },
 
   {
-    id: 5,
+    id: 6,
     title: "The Moderator Website",
     description: "A simple and responsive website to display information about The Moderator Bot.",
     image: "/Images/Projects/DiscordWebsite.png",
@@ -52,7 +61,7 @@ const projectsData = [
     demoUrl: "https://themoderator.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     title: "Portfolio Website",
     description: "Welcome to the heart of my digital journey - an immersive showcase that unveils the code powering my portfolio website.",
     image: "/Images/Projects/PortfolioWebsite_adj.png",
@@ -61,7 +70,7 @@ const projectsData = [
     demoUrl: "https://kenetortiz.vercel.app/",
   },
   {
-    id: 7,
+    id: 8,
     title: "Wordle",
     description: "Dynamic Wordle game project featuring JavaScript with Node.js, boasting authentication, Firebase API integration.",
     image: "/Images/Projects/Wordle.png",
@@ -70,16 +79,16 @@ const projectsData = [
     demoUrl: "https://wordledesktop.vercel.app/",
   },
   {
-    id: 8,
-    title: "Invoker Trainer",
-    description: "Embark on a thrilling journey to master the arcane arts of Invoker, the iconic hero in Dota 2, with our compact and engaging skill improvement game. Designed for both novice and seasoned players, the Invoker Mastery Challenge offers a unique and entertaining experience to enhance your proficiency with this powerful hero.",
-    image: "/Images/Projects/Dota2_adj.png",
+    id: 9,
+    title: "Result Sentry",
+    description: "ResultSentry is a Python-based web scraper that fetches soccer match results for a specific team from ESPN's website and saves the data into a clean, structured json file.",
+    image: "/Images/Projects/ResultSentry.png",
     tag: ["All", "Python"],
-    gitUrl: "https://github.com/KOrtizLedezma/InvokerTrainer",
+    gitUrl: "https://github.com/KOrtizLedezma/ResultSentry.git", 
     demoUrl: null,
   },
   {
-    id: 9,
+    id: 10,
     title: "Random Mail Gift Mod",
     description: "Introducing the \"Random Mail Gift\" mod, a delightful addition to Stardew Valley that adds an element of surprise to your daily routine. This mod is designed to bring joy to players by sending carefully curated gifts each day, tailored to your in-game luck. Say goodbye to mundane routines and hello to daily surprises that will brighten your virtual farm life",
     image: "/Images/Projects/StardewValleyMod_adj.png",
@@ -88,7 +97,7 @@ const projectsData = [
     demoUrl: null,
   },
   {
-    id: 10,
+    id: 11,
     title: "DoorLock SV Mod",
     description: "A Stardew Valley mod that lets players lock doors to control access, using in-game data for automatic cabin assignments.",
     image: "/Images/Projects/DoorLockSV.png",
@@ -97,7 +106,7 @@ const projectsData = [
     demoUrl: null,
   },
   {
-    id: 11,
+    id: 12,
     title: "Racing Cars Game",
     description: "Classic Arcade game where the player needs to avoid the incoming traffic and get the highest score possible, allows local multiplayer",
     image: "/Images/Projects/Cars.png",
@@ -106,21 +115,12 @@ const projectsData = [
     demoUrl: null,
   },
   {
-    id: 12,
-    title: "Sudoku",
-    description: "Classic Sudoku Game, This project showcases my proficiency in Python programming, offering players a delightful experience with various difficulty levels—Easy, Medium, and Hard.",
-    image: "/Images/Projects/Sudoku.png",
-    tag: ["All", "Python"],
-    gitUrl: "https://github.com/KOrtizLedezma/Sudoku-Project-4.git",
-    demoUrl: null,
-  },
-  {
     id: 13,
-    title: "Minesweeper",
-    description: "Minesweeper, a classic game with a strategic twist, stands as a testament to my programming skills and passion for game development. In this project, implemented using C++, players embark on an adventure to uncover hidden mines on a grid while avoiding detonation.",
-    image: "/Images/Projects/Minesweeper.png",
-    tag: ["All", "C++"],
-    gitUrl: "https://github.com/KOrtizLedezma/Minesweeper.git",
+    title: "Soccer Predictor",
+    description: "Soccer Predictor is a simple application designed to analyze and predict soccer match results using historical data.",
+    image: "/Images/Projects/soccer_predictor.png",
+    tag: ["All", "Python"],
+    gitUrl: "https://github.com/KOrtizLedezma/SoccerPredictor.git", 
     demoUrl: null,
   },
   {
@@ -132,6 +132,34 @@ const projectsData = [
     gitUrl: "https://github.com/KOrtizLedezma/To-Do-List.git", 
     demoUrl: null,
   },
+  {
+    id: 15,
+    title: "Sudoku",
+    description: "Classic Sudoku Game, This project showcases my proficiency in Python programming, offering players a delightful experience with various difficulty levels—Easy, Medium, and Hard.",
+    image: "/Images/Projects/Sudoku.png",
+    tag: ["All", "Python"],
+    gitUrl: "https://github.com/KOrtizLedezma/Sudoku-Project-4.git",
+    demoUrl: null,
+  },
+  {
+    id: 16,
+    title: "Invoker Trainer",
+    description: "Embark on a thrilling journey to master the arcane arts of Invoker, the iconic hero in Dota 2, with our compact and engaging skill improvement game. Designed for both novice and seasoned players, the Invoker Mastery Challenge offers a unique and entertaining experience to enhance your proficiency with this powerful hero.",
+    image: "/Images/Projects/Dota2_adj.png",
+    tag: ["All", "Python"],
+    gitUrl: "https://github.com/KOrtizLedezma/InvokerTrainer",
+    demoUrl: null,
+  },
+  {
+    id: 17,
+    title: "Minesweeper",
+    description: "Minesweeper, a classic game with a strategic twist, stands as a testament to my programming skills and passion for game development. In this project, implemented using C++, players embark on an adventure to uncover hidden mines on a grid while avoiding detonation.",
+    image: "/Images/Projects/Minesweeper.png",
+    tag: ["All", "C++"],
+    gitUrl: "https://github.com/KOrtizLedezma/Minesweeper.git",
+    demoUrl: null,
+  },
+  
 ];
 
 const ProjectsSection = () => {
